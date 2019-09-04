9dd905e637f6b714967c940d916320fc81b8e8dd
tolmatol <tolmatol2016@gmail.com>
2019-09-05 05:28:22 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent  { 
+  pageTitle = "List of courses";
 }

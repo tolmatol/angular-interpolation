61620a2385077bd3d572e1b75e2e270151a11dc4
tolmatol <tolmatol2016@gmail.com>
2019-09-05 06:16:53 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  { 
-  pageTitle = "List of courses";
+  pageTitle = "List of students";
 }
